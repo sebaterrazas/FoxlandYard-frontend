47689ac383f4dd189f02b2c8c738a633532b7512
Nacho <ignacio.lillo@uc.cl>
2023-04-20 23:19:52 -0400

    Commit 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import LandingPage from './views/LandingPage'
+import AboutUs from './views/AboutUs'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App />
+    <UserWelcome /> */}
+    {/* <LandingPage/> */}
+    <AboutUs/>
   </React.StrictMode>,
 )
--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,11 +1,36 @@
 import { useState } from 'react'
+import '../styles/AboutUs.css'
 
 function AboutUs() {
 
   return (
-    <div className="AboutUs">
+    // <div className="AboutUs">
+    //     <h1>Bienvenido a About Us</h1>
+    // </div>
+    <>
+    <h1 class="titulo" > ¡Acerca del equipo!</h1>
 
-    </div>
+    <section class="flex-box-seba">
+        <p class="nombre"> Sebastián Terrazas </p>
+        <div class="contenedor-flexible">
+            <img class="avatar" src="../../src/assets/images/MrFox.jpg"></img>
+            <p class="descripcion"> Es una persona graciosa que le gusta patinar en hielo. </p>
+        </div>
+    </section>
+
+    <section class="flex-box-nacho">
+        <p class="nombre"> Ignacio Lillo </p>
+        <div class="contenedor-flexible">
+            <img class="avatar" src="../../src/assets/images/Ash1.jpg"></img>
+            <p class="descripcion"> Es una persona que le gusta comer pasteles y montar a caballo. </p>
+        </div>
+    </section>
+
+    <section class="flex-box-botones">
+        <p> Aca van los botones para cambiar de páginas </p>
+    </section>
+
+    </>
   )
 }
 
