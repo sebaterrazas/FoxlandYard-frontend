7760fd7c13d287e685ec18e261be9a36f826425f
SEBASTIAN TERRAZAS <sebaterrazas@uc.cl>
2023-04-21 17:32:01 -0400

    fix conflict

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -7,10 +7,6 @@ function LandingPage() {
     <div className="LandingPage">
         <h1> ¡Bienvenido a Foxland Yard! </h1>
         <img id="logo" src="../../src/assets/images/Logo1.png"></img>
-<<<<<<< HEAD
-        
-        <p>Gracias por visitar nuestro sitio web. Aquí podrás encontrar información útil sobre nuestro proyecto.</p>
-=======
         <p id="gracias">¡Gracias por visitar nuestro sitio web! Aquí podrás encontrar información útil sobre nuestro proyecto.</p>
 
         <section className="flex_como_jugar">
@@ -18,7 +14,6 @@ function LandingPage() {
           <p id="explicacion_juego">Este es un emocionante juego de estrategia, astucia y cooperación entre los jugadores. Conviértete en Mr Fox y evita ser capturado por los granjeros, o trabaja en equipo con otros granjeros para atrapar a Mr Fox.</p>
           <img id="imagen2" src="../../src/assets/images/mockup_antiguo.jpg"></img>
         </section>
->>>>>>> Nacho
 
         <Login />
 
