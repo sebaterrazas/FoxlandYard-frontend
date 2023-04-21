babadcd2c7fb14fd6a9a3ee2beee39e1b8767c05
Nacho <ignacio.lillo@uc.cl>
2023-04-21 16:35:49 -0400

    Un poco del avance del día viernes

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -7,17 +7,22 @@ function LandingPage() {
     <div className="LandingPage">
         <h1> ¡Bienvenido a Foxland Yard! </h1>
         <img id="logo" src="../../src/assets/images/Logo1.png"></img>
-        <p>Gracias por visitar nuestro sitio web. Aquí podrás encontrar información útil sobre nuestro proyecto.</p>
-        <p>IMAGEN QUE TENGA RELACION CON ESTE</p>
+        <p id="gracias">¡Gracias por visitar nuestro sitio web! Aquí podrás encontrar información útil sobre nuestro proyecto.</p>
+
+        <section className="flex_como_jugar">
+          <h2 id="como_jugar"> Cómo se juega: </h2>
+          <p id="explicacion_juego">Este es un emocionante juego de estrategia, astucia y cooperación entre los jugadores. Conviértete en Mr Fox y evita ser capturado por los granjeros, o trabaja en equipo con otros granjeros para atrapar a Mr Fox.</p>
+          <img id="imagen2" src="../../src/assets/images/mockup_antiguo.jpg"></img>
+        </section>
 
         <Login />
 
-        <p>Crea una cuenta!</p>
+        {/* <p>Crea una cuenta!</p>
         <form>
             <input type="text" placeholder="Ingresa tu correo electrónico" />
             <input type="text" placeholder="Ingresa tu futura clave" />
             <button type="submit">Suscríbete</button>
-        </form>
+        </form> */}
     </div>
   );
 }
