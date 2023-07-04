7dd74dbc488bd4512c7a1a2d0c5eb9d1a363c39b
SEBASTIAN TERRAZAS <sebaterrazas@uc.cl>
2023-07-04 18:59:10 -0400

    redirects

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:1234',
+  baseURL: 'https://foxlandyard-api.onrender.com',
 });
 
 export default instance;
